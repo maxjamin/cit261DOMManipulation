bec1290e057b67bf852031b6bb3d974ce8c1493f
maxjamin <maxjamin@gmail.com>
2019-01-26 12:27:20 -0700

    Added function addToFront

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+
+var incrText = 3;
+
 function addBook() {
 	//append child()
 	var input = document.getElementById("bookName").value;
-
+	var author = document.getElementById("authorName").value;
+	var textBox = document.getElementById("textBox").value;
 	var checkList= document.getElementById("books");
+	console.log(text);
 
 	if(input != "")
 	{
@@ -12,15 +17,37 @@ function addBook() {
 		node.appendChild(text);
 		node.id = input;
 
+		var node01 = document.createElement("UL");
+		var text01 = document.createTextNode(" ");
+		node01.appendChild(text01);
+
+		var node02 = document.createElement("LI");
+		var text02 = document.createTextNode(author);
+		node02.appendChild(text02);
+		node02.id = author;
+
+		var node03 = document.createElement("LI");
+		var text03 = document.createTextNode(textBox);
+		node03.appendChild(text03);
+		node03.id = "textBox" + incrText;
+
+		node01.appendChild(node02);
+		node01.appendChild(node03);
+		node.appendChild(node01);
 		checkList.appendChild(node);
+
 	}
 	console.log(checkList.innerHTML);
+	incrText += 1;
 }
 
 
 function addToFront() {
 
 	var input = document.getElementById("bookName").value;
+	var author = document.getElementById("authorName").value;
+	var textBox = document.getElementById("textBox").value;
+	var checkList= document.getElementById("books");
 
 	var checkList= document.getElementById("books");
 
@@ -31,6 +58,24 @@ function addToFront() {
 		var text = document.createTextNode(input);
 		node.appendChild(text);
 		node.id = input;
+
+		var node01 = document.createElement("UL");
+		var text01 = document.createTextNode(" ");
+		node01.appendChild(text01);
+
+		var node02 = document.createElement("LI");
+		var text02 = document.createTextNode(author);
+		node02.appendChild(text02);
+		node02.id = author;
+
+		var node03 = document.createElement("LI");
+		var text03 = document.createTextNode(textBox);
+		node03.appendChild(text03);
+		node03.id = "textBox" + incrText;
+
+		node01.appendChild(node02);
+		node01.appendChild(node03);
+		node.appendChild(node01);
 
 		checkList.insertBefore(node, checkList.childNodes[0]);
 	}
@@ -53,8 +98,6 @@ function removeBook()
 		checkList.removeChild(one);
 
 	}
-
-
 
 
 	console.log(checkList.innerHTML);
