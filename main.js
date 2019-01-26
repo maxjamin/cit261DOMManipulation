b1cbb0e9558c75c4fcc79eddfd8c4be21db37315
maxjamin <maxjamin@gmail.com>
2019-01-26 11:25:19 -0700

    changed files

---
+++ main.js
@@ -0,0 +1,43 @@
+function addBook() {
+	//append child()
+	var input = document.getElementById("bookName").value;
+
+	var checkList= document.getElementById("books");
+
+	if(input != "")
+	{
+
+		var node = document.createElement("LI");
+		var text = document.createTextNode(input);
+		node.appendChild(text);
+		node.id = input;
+
+		checkList.appendChild(node);
+	}
+	console.log(checkList.innerHTML);
+}
+
+
+
+function removeBook()
+{
+	var checkList = document.getElementById("books");
+	var input = document.getElementById("removeName").value;
+	var one = document.getElementById(input);
+
+	console.log(input);
+	console.log(one);
+
+	//checkList if books has a child node.
+	if (checkList.hasChildNodes() && (one != null)) {
+		console.log("test01")
+		checkList.removeChild(one);
+
+	}
+
+
+
+
+	console.log(checkList.innerHTML);
+	
+}
